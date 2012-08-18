b3985a093eaf9b065f2d597b4f879573da5afe3b
Christopher Reimer <c.reimer1993@googlemail.com>
2012-08-18 14:07:20 +0200

    vdr: Set ACPI wakeup without init hook to prepare for a future switch to systemd

--- PKGBUILDs/vdr/shutdown.sh
+++ PKGBUILDs/vdr/shutdown.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-/bin/echo $1 > /tmp/.wakeup
-/sbin/halt
+#Unset previous timer
+echo '0' > /sys/class/rtc/rtc0/wakealarm
 
+#Set new timer 5mins before recording starts
+echo $(($1 - 300 )) > /sys/class/rtc/rtc0/wakealarm
+
+#Initiate shutdown
+/sbin/halt
