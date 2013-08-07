df8d26405bf7d58582b08540fa45060d2bf699a0
Christopher Reimer <mail@creimer.net>
2013-08-07 20:11:01 +0200

    Add vdr-addon-lifeguard

--- vdr/shutdown.sh
+++ vdr/shutdown.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+#Run through shutdown-hooks
+for file in /usr/share/vdr/shutdown-hooks/*; do
+  if [ -x $file ]; then
+    MESSAGE=$($file)
+    if [ $? != '0' ]; then
+      MESSAGE=$(echo "$MESSAGE" | sed -rn '0,/ABORT_MESSAGE/s/^ABORT_MESSAGE="?([^"]+).*/\1/p')
+      svdrpsend MESG "$MESSAGE"
+      exit 1
+    fi
+  fi
+done
+
 #Sync back system time to hardware time (Maybe VDR has changed it)
 hwclock --systohc --utc
 
