1e2c26cf863ba514c988e62fc5ae52ffaafa7738
Tomasz Maciej Nowak <tomek_n@o2.pl>
2019-07-16 21:36:59 +0200

    Enable Travis CI
    Use one of freely available continuous integration services to test
    commits and pull requests for build fails. The build logs are visible
    for everyone to inspect what went wrong and help to troubleshoot.
    
    Since Travis CI handles building of the packages, let's use them and
    create backup package repository using GitHub pages. The url to view
    listing will be: https://VDR4Arch.github.io/vdr4arch. The snippet for
    pacman.conf will look like this:
    
    [vdr4arch]
    SigLevel = Never TrustAll
    Server = https://VDR4Arch.github.io/$repo/$arch
    
    With this we can create something similar to Ubuntu PPA. Unfortunately
    the rebuilds (on soname bumps) still will need to be handled manually
    with commit to repository, changing pkgrel.

---
+++ .ci/genindex.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+target="$1"
+repo="$2"
+
+create_index() {
+	index=$(ls --color=never --group-directories-first -1 -p --hide=index.html $1 | awk '{print "<a href=\""$1"\">"$1"</a>"}')
+
+	echo "<html>
+<head><title>Index of /${r}${d}</title></head>
+<body>
+<h1>Index of /${r}${d}</h1><hr><pre>
+<a href="../">../</a>
+${index}
+</pre><hr></body>
+</html>" > index.html
+}
+
+cd ${target}
+
+d=${repo}/
+create_index
+r=${d}
+
+for d in */; do
+	(cd ${d}
+	create_index)
+done
