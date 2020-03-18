dbb338256fe7accb48fca22c50d3af778657bcbd
Manuel Reimer <manuel.reimer@gmx.de>
2020-03-18 16:42:09 +0100

    Specify DOCTYPE for index.html, remove leading slash in title

--- .ci/genindex.sh
+++ .ci/genindex.sh
@@ -4,12 +4,13 @@ target="$1"
 repo="$2"
 
 create_index() {
-	index=$(ls --color=never --group-directories-first -1 -p --hide=index.html $1 | awk '{print "<a href=\""$1"\">"$1"</a>"}')
+  index=$(ls --color=never --group-directories-first -1 -p --hide=index.html $1 | awk '{print "<a href=\""$1"\">"$1"</a>"}')
 
-	echo "<html>
-<head><title>Index of /${r}${d}</title></head>
+  echo "<!DOCTYPE html>
+<html>
+<head><title>Index of ${r}${d}</title></head>
 <body>
-<h1>Index of /${r}${d}</h1><hr><pre>
+<h1>Index of ${r}${d}</h1><hr><pre>
 <a href="../">../</a>
 ${index}
 </pre><hr></body>
@@ -23,6 +24,6 @@ create_index
 r=${d}
 
 for d in */; do
-	(cd ${d}
-	create_index)
+  (cd ${d}
+  create_index)
 done
